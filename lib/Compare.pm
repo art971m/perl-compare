4ab61be9dc811bc11d7d4d3224cf81864028b029
Artem Melnyk <art971m@gmail.com>
2018-09-22 14:28:01 +0200

    get_array_diff

---
+++ lib/Compare.pm
@@ -0,0 +1,31 @@
+package Compare;
+use Data::Dumper;
+
+
+sub get_array_diff {
+    my ($self, $arr_ref_db, $arr_ref_dump) = @_;
+
+    my @arr_db = @{$arr_ref_db};
+    my @arr_dump = @{$arr_ref_dump};
+
+    my %missing;
+    undef @missing{@arr_db};
+    delete @missing{@arr_dump};
+    my @missing_values = keys %missing;
+
+    my %unexpected;
+    undef @unexpected{@arr_dump};
+    delete @unexpected{@arr_db};
+    my @unexpected_values = keys %unexpected;
+
+    return (\@missing_values, \@unexpected_values);
+}
+
+sub get_discrepancy {
+}
+
+
+
+
+
+1;
---
+++ t/compare.t
@@ -0,0 +1,10 @@
+use lib '../lib';
+use Compare;
+
+use Test::More tests => 2;
+
+my @arr_db = (1, 2, 3, 4);
+my @arr_dump = (3, 4, 5, 6);
+my ($missing_values, $unexpected_values) = Compare->get_array_diff(\@arr_db, \@arr_dump);
+is_deeply([sort @{$missing_values}], [1, 2], 'missing values');
+is_deeply([sort @{$unexpected_values}], [5, 6], 'unexpected values');
