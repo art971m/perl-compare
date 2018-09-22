a1b2565edbef4b372facfe69ca8a24d367e7f7b6
Artem Melnyk <art971m@gmail.com>
2018-09-22 19:54:13 +0200

    impr comp

--- lib/Compare.pm
+++ lib/Compare.pm
@@ -3,27 +3,35 @@ package Compare;
 sub get_array_diff {
     my ($self, $arr_ref_db, $arr_ref_dump) = @_;
 
-    my @arr_db = @{$arr_ref_db};
-    my @arr_dump = @{$arr_ref_dump};
+    my @arr_db = (defined $arr_ref_db) 
+        ? @{$arr_ref_db}
+        : ();
+    my @arr_dump = (defined $arr_ref_dump) 
+        ? @{$arr_ref_dump}
+        : ();
 
-    my %missing;
-    undef @missing{@arr_db};
-    delete @missing{@arr_dump};
-    my @missing_values = sort keys %missing;
+    my ($missing_values, $unexpected_values);
+    if ((scalar @arr_db) && (scalar @arr_dump)) {
+        my %missing;
+        undef @missing{@arr_db};
+        delete @missing{@arr_dump};
+        $missing_values = (%missing)
+            ? [sort keys %missing]
+            : undef;
 
-    my %unexpected;
-    undef @unexpected{@arr_dump};
-    delete @unexpected{@arr_db};
-    my @unexpected_values = sort keys %unexpected;
+        my %unexpected;
+        undef @unexpected{@arr_dump};
+        delete @unexpected{@arr_db};
+        $unexpected_values = (%unexpected)
+            ? [sort keys %unexpected]
+            : undef;
+    } elsif ((scalar @arr_db) && (!scalar @arr_dump)) {
+        $missing_values = [sort @arr_db];
+    } elsif ((!scalar @arr_db) && (scalar @arr_dump)) {
+        $unexpected_values = [sort @arr_dump];
+    }
 
-    return (\@missing_values, \@unexpected_values);
+    return ($missing_values, $unexpected_values);
 }
-
-sub get_discrepancy {
-}
-
-
-
-
 
 1;
--- t/compare.t
+++ t/compare.t
@@ -1,8 +1,28 @@
 use Compare;
-use Test::More tests => 2;
+use Test::More tests => 12;
 
 my @arr_db = (1, 2, 3, 4);
 my @arr_dump = (3, 4, 5, 6);
 my ($missing_values, $unexpected_values) = Compare->get_array_diff(\@arr_db, \@arr_dump);
 is_deeply($missing_values, [1, 2], 'missing values');
 is_deeply($unexpected_values, [5, 6], 'unexpected values');
+
+($missing_values, $unexpected_values) = Compare->get_array_diff([], \@arr_dump);
+is_deeply($missing_values, undef, 'missing values');
+is_deeply($unexpected_values, [3, 4, 5, 6], 'unexpected values');
+
+($missing_values, $unexpected_values) = Compare->get_array_diff(undef, \@arr_dump);
+is_deeply($missing_values, undef, 'missing values');
+is_deeply($unexpected_values, [3, 4, 5, 6], 'unexpected values');
+
+($missing_values, $unexpected_values) = Compare->get_array_diff(\@arr_db, []);
+is_deeply($missing_values, [1, 2, 3, 4], 'missing values');
+is_deeply($unexpected_values, undef, 'unexpected values');
+
+($missing_values, $unexpected_values) = Compare->get_array_diff(\@arr_db, undef);
+is_deeply($missing_values, [1, 2, 3, 4], 'missing values');
+is_deeply($unexpected_values, undef, 'unexpected values');
+
+($missing_values, $unexpected_values) = Compare->get_array_diff(undef, undef);
+is_deeply($missing_values, undef, 'missing values');
+is_deeply($unexpected_values, undef, 'unexpected values');
